c5b15384e8b11761beb68b9c4dda31670d505a61
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-29 15:53:16 -0600

    create CSS files for header and footer ;

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
+import './footer.css';
 
 function Footer () {
   return (
     <>
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar className="Navbar" bg="dark" variant="dark" expand="lg">
       <Navbar.Brand id="navBarFooterTitle">To Do App</Navbar.Brand>
     </Navbar>
     </>
