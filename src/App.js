1fb30566e43a0069b34881b27622b5c12a7efd42
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-29 14:29:21 -0600

    Create Home and About components and import them in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
