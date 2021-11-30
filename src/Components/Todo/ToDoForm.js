7f2b62c6b721bd0207f57a08355917ffb3782420
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-30 13:52:14 -0600

    try to make form work with state

--- src/Components/Todo/ToDoForm.js
+++ src/Components/Todo/ToDoForm.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import { Form, Card, Button } from 'react-bootstrap';
 
-function ToDoForm() {
+function ToDoForm(props) {
+
+  function handleSubmit(event) {
+    event.preventDefautlt();
+
+    const form = event.target;
+    const { title, assignedTo, difficulty } = form.elements;
+
+    const formData = {
+      title,
+      assignedTo,
+      difficulty,
+    };
+
+    console.log(formData);
+
+    event.target.reset();
+    event.targe.elements.title.focus();
+  }
+
   return (
     <>
       <Card className="mt-4" style={{ width: '25rem' }}>
         <Card.Title className="mb-4">Add To Do Item</Card.Title>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-4">
             <Form.Label>To Do Item</Form.Label>
-            <Form.Control placeholder="Details" />
+            <Form.Control type="text" name="title" placeholder="Details" />
           </Form.Group>
           <Form.Group className="mb-4">
             <Form.Label>Assigned To</Form.Label>
-            <Form.Control placeholder="Name" />
+            <Form.Control type="text" name="assignedTo" placeholder="Name" />
           </Form.Group>
           <Form.Group className="mb-4">
             <Form.Label>Difficulty</Form.Label>
-            <Form.Range min="1" max="10" />
+            <Form.Range name="difficulty" min="1" max="10" />
           </Form.Group>
-          <Button className="mb-4" variant="secondary" size="sm">Add Item</Button>
+          <Button className="mb-4" variant="secondary" size="sm" type="submit">Add Item</Button>
         </Form>
       </Card>
     </>
