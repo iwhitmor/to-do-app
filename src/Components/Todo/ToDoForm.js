fbde0ce73574396d1241c34d83f0bd3fbae45d02
Keith Dahlby <keith@deltavcodeschool.com>
2021-11-30 10:33:37 -0600

    Add Card.Body and adjust layout

--- src/Components/Todo/ToDoForm.js
+++ src/Components/Todo/ToDoForm.js
@@ -4,23 +4,25 @@ import { Form, Card, Button } from 'react-bootstrap';
 function ToDoForm() {
   return (
     <>
-      <Card className="mt-4" style={{ width: '25rem' }}>
-        <Card.Title className="mb-4">Add To Do Item</Card.Title>
-        <Form>
-          <Form.Group className="mb-4">
-            <Form.Label>To Do Item</Form.Label>
-            <Form.Control placeholder="Details" />
-          </Form.Group>
-          <Form.Group className="mb-4">
-            <Form.Label>Assigned To</Form.Label>
-            <Form.Control placeholder="Name" />
-          </Form.Group>
-          <Form.Group className="mb-4">
-            <Form.Label>Difficulty</Form.Label>
-            <Form.Range min="1" max="10" />
-          </Form.Group>
-          <Button className="mb-4" variant="secondary" size="sm">Add Item</Button>
-        </Form>
+      <Card className="mt-4">
+        <Card.Body>
+          <Card.Title className="mb-4">Add To Do Item</Card.Title>
+          <Form>
+            <Form.Group className="mb-4">
+              <Form.Label>To Do Item</Form.Label>
+              <Form.Control placeholder="Details" />
+            </Form.Group>
+            <Form.Group className="mb-4">
+              <Form.Label>Assigned To</Form.Label>
+              <Form.Control placeholder="Name" />
+            </Form.Group>
+            <Form.Group className="mb-4">
+              <Form.Label>Difficulty</Form.Label>
+              <Form.Range min="1" max="10" />
+            </Form.Group>
+            <Button className="mb-4" variant="secondary" size="sm">Add Item</Button>
+          </Form>
+        </Card.Body>
       </Card>
     </>
   )
