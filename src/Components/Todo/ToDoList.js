bfb2225d85be82c506cab0134a0e1cc1b1197d3a
Ian Whitmore <ianwhitmor@gmail.com>
2021-11-30 10:16:44 -0600

    auto formatting

--- src/Components/Todo/ToDoList.js
+++ src/Components/Todo/ToDoList.js
@@ -2,30 +2,30 @@ import { prependOnceListener } from 'process';
 import React from 'react';
 import { Badge, Card, ListGroup, CloseButton } from 'react-bootstrap';
 
-function ToDoList(props){
-  
+function ToDoList(props) {
+
   const { data } = props;
-  
+
   return (
-  <>
-   {data.map(task => (
-     <Card className="mt-4" style={{ width: '32rem' }} key={task.title}>
-       <Card.Header>
-         {task.completed ? <Badge pill bg="success">Complete</Badge> : <Badge pill bg="danger">Pending</Badge>} 
-         {task.assignedTo}
-         <CloseButton />
-       </Card.Header> 
-       <Card.Body>
-       <Card.Title>
-        {task.title} 
-        </Card.Title>
-        <Card.Text>
-        Difficulty: {task.difficulty}
-        </Card.Text>
-       </Card.Body>
-       </Card>
-   ))}
-  </>  
+    <>
+      {data.map(task => (
+        <Card className="mt-4" style={{ width: '32rem' }} key={task.title}>
+          <Card.Header>
+            {task.completed ? <Badge pill bg="success">Complete</Badge> : <Badge pill bg="danger">Pending</Badge>}
+            {task.assignedTo}
+            <CloseButton />
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>
+              {task.title}
+            </Card.Title>
+            <Card.Text>
+              Difficulty: {task.difficulty}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
+    </>
   )
 }
 
