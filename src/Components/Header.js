b1900c5e76d65bf6eb56969f3cb088f87c8f6d16
Ian Whitmore <ianwhitmor@gmail.com>
2021-12-02 09:54:26 -0600

    change navbar link colors and text to white

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,15 +10,15 @@ function Header() {
 
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="primary" variant="dark">
         <Container>
-          <Nav.Link as={Link} to="/">Home</Nav.Link>
-          <Nav.Link as={Link} to="/about">About</Nav.Link>
+          <Nav.Link style={{ color: "white" }} as={Link} to="/">Home</Nav.Link>
+          <Nav.Link style={{ color: "white" }} as={Link} to="/about">About</Nav.Link>
           <Navbar.Collapse className="justify-content-end">
             {!user && <Login />}
             {user &&
               <>
-                <Navbar.Text>Welcome back, {user.username}</Navbar.Text>
+                <Navbar.Text style={{ color: "white" }}>Welcome back, {user.username}</Navbar.Text>
                 <Navbar.Text>
                   <Button onClick={() => logout()} variant="dark">Log Out</Button>
                 </Navbar.Text>
